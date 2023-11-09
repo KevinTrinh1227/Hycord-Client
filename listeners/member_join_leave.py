2bdadf648501ea5d20c0533e1c6082ab68ac8ae8
KevinTrinh1227 <kevintrinh1227@gmail.com>
2023-11-08 22:12:36 -0600

    merged both log listeners in same file

--- listeners/member_join_leave.py
+++ listeners/member_join_leave.py
@@ -17,10 +17,10 @@ join_dm_template = data["embed_templates"]["join_dm_message"]
 #json data to run bot
 bot_prefix = data["general"]["bot_prefix"]
 embed_color = int(data["general"]["embed_color"].strip("#"), 16) #convert hex color to hexadecimal format
-leave_channel_id = int(data["text_channel_ids"]["bot_logs"])    # logs the bot logs in this channel
 member_role_id = int(data["role_ids"]["unverified_member"])
 welcome_channel_id = int(data["text_channel_ids"]["welcome"])
 pre_embed_color = data["general"]["embed_color"].strip("#")
+logs_channel_id = int(data["text_channel_ids"]["bot_logs"])    # logs the bot logs in this channel
 
 font_title = ImageFont.truetype("./assets/fonts/Minecraft.ttf", 20)
 font_footer = ImageFont.truetype("./assets/fonts/Minecraft.ttf", 15)
@@ -31,7 +31,7 @@ class joinleave(commands.Cog):
         self.client = client
         
         
-    #on member join event        
+    # Welcome message. This will not be in the logs channel.
     @commands.Cog.listener()
     async def on_member_join(self, member):
         
@@ -174,11 +174,11 @@ class joinleave(commands.Cog):
         await member.add_roles(autoRole)
     
 
-    # on member leave event
+    # logs when someone leaves the server
     @commands.Cog.listener()
     async def on_member_remove(self, member):
         
-        channel = self.client.get_channel(leave_channel_id)
+        channel = self.client.get_channel(logs_channel_id)
         embed = discord.Embed(
             title=(f"{member.display_name} has left the server."),
             description=f"{member.mention} aka `{member.name}` has left {member.guild.name}.",
@@ -191,6 +191,61 @@ class joinleave(commands.Cog):
         embed.timestamp = datetime.datetime.now()
         embed.set_footer(text=f"©️ {member.guild.name}", icon_url = member.guild.icon.url)
         await channel.send(embed=embed)
+        
+        
+    # logs when someone deletes a message
+    @commands.Cog.listener()
+    async def on_message_delete(self, message):
+        async for entry in message.guild.audit_logs(limit=1, action=discord.AuditLogAction.message_delete):
+            deleter = entry.user
+            
+            #print(entry)
+            #print(message)
+        
+            channel = self.client.get_channel(logs_channel_id)
+        
+            embed = discord.Embed(
+                title=(f"🗑️ | A message was deleted in #{message.channel.name}"),
+                description=f"**Message deleted:** ```{message.content}```",
+                colour= embed_color
+                )
+            embed.set_author(name=f"{deleter.name} ({deleter.display_name})", icon_url=deleter.avatar.url)
+            embed.add_field(name="Message Author", value=message.author.mention,
+                            inline=True)
+            embed.add_field(name="Author Name", value=message.author.name,
+                            inline=True)
+            embed.add_field(name="Author ID", value=message.author.id,
+                            inline=True)
+            
+            embed.add_field(name="Deleter", value=deleter.mention,
+                            inline=True)
+            embed.add_field(name="Deleter Name", value=deleter.name,
+                            inline=True)
+            embed.add_field(name="Deleter ID", value=deleter.id,
+                            inline=True)
+
+            await channel.send(embed=embed)
+
+
+    # logs when someone edits a message
+    @commands.Cog.listener()
+    async def on_message_edit(self, message_before, message_after):
+        embed = discord.Embed(
+            title=f"✂️ | A message was edited in #{message_before.channel.name}",
+            description=f"**Old Message:**\n```{message_before.content}```\n**New Message:**\n```{message_after.content} ```",
+            color=0xFF0000
+            )
+        embed.set_author(name=f"{message_before.author.name} ({message_before.author.display_name})", icon_url=message_before.author.avatar.url)
+        embed.add_field(name="Message Author", value=message_before.author.mention,
+                        inline=True)
+        embed.add_field(name="Author Name", value=message_before.author.name,
+                        inline=True)
+        embed.add_field(name="Author ID", value=message_before.author.id,
+                        inline=True)
+        
+        channel = self.client.get_channel(logs_channel_id)
+        
+        await channel.send(channel, embed=embed)
 
         
 async def setup(client):
